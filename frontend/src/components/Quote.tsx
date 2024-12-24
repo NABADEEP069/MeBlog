40fb4ca76441dd6bb47e7dd83821de9c528e74aa
Nabadeep <nabadeepdas069@gmail.com>
2024-12-24 14:34:59 +0530

    homepage

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () => {
+ return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+    <div className="flex justify-center">
+    <div  className="max-w-md "> 
+        <div className="text-3xl font-bold">
+        Welcome to "MeBlog" — Your haven for thoughts, stories, and endless inspiration.
+    </div>
+    <div className="max-w-md text-xl font-semibold text-left mt-4">
+        Nabadeep Kr. das
+        </div>
+
+     <div className="max-w-md text-sm font-light text-slate-400">
+        Software Engineer | CITK CS'26
+        
+     </div>
+
+    </div>
+ </div>
+ </div>
+}
