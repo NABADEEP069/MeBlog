77fe176eee00555e414d7dfdc5f7460f198920b8
Nabadeep <nabadeepdas069@gmail.com>
2024-12-25 22:13:13 +0530

    ./

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs';
 import './App.css'
+
+
 
 function App() {
   
@@ -14,6 +17,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,7 @@
+export const Blogs = () => {
+    return (
+        <div>
+        <h1>Blogffffffffffffffs</h1>
+        </div>
+    )
+    }
