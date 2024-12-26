3a6a0589ece3c1988f2bcffc9cc105b56e789114
Nabadeep <nabadeepdas069@gmail.com>
2024-12-26 14:33:32 +0530

    update frontend

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -59,6 +59,9 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                         })
                     }} />
                     <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                    <button onClick={() => {
+                        navigate("/blogs")
+                    }} type="button" className="mt-4 w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-900 dark:focus:ring-blue-900 dark:border-blue-900">Troblue while signup ? Continue as guest</button>
                 </div>
             </div>
         </div>
